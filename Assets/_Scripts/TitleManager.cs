a93ae066fcce3446c1b6f7b0a95fe9d07354f7bf
ElliottFutureGames <elliot.fagerdin.blom@edu.futuregames.nu>
2023-10-26 11:27:27 +0200

    Made game start by pressing any one of the big buttons

--- Assets/_Scripts/TitleManager.cs
+++ Assets/_Scripts/TitleManager.cs
@@ -7,9 +7,26 @@ using UnityEngine.SceneManagement;
 public class TitleManager : MonoBehaviour
 {
     [SerializeField] string gameScene;
-
+    PlayerInputActions playerInputActions;
     public void LoadGame()
     {
+        Debug.Log("Loading");
         SceneManager.LoadScene(gameScene);
+    }
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Enable();
+    }
+    private void Update()
+    {
+        if (playerInputActions.Player.dpadDownLeft.IsPressed()  || 
+            playerInputActions.Player.dpadUpLeft.IsPressed()    || 
+            playerInputActions.Player.dpadDownRight.IsPressed() ||
+            playerInputActions.Player.dpadUpRight.IsPressed()   )
+        {
+            LoadGame();
+        }
     }
 }
