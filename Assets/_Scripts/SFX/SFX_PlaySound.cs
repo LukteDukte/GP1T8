05ecda12fbf3bc83e7185a6a34c0f1c79d8962e3
Markus Vikmanis <markus.tigern@gmail.com>
2023-10-26 15:08:17 +0200

    Added in the SFX files and WIP on the audio Script.

---
+++ Assets/_Scripts/SFX/SFX_PlaySound.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX_PlaySound : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
