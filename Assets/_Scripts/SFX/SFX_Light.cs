51ddb8a664dc843691a32d269d54c2c5d2f724c7
LukteDukte <markus.tigern@gmail.com>
2023-10-31 11:03:15 +0100

    wip on the sfx update implementation

--- Assets/_Scripts/SFX/SFX_Light.cs
+++ Assets/_Scripts/SFX/SFX_Light.cs
@@ -4,11 +4,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+[Serializable]
+public class SFX_AudioClips
+{
+    public AudioClip Clip;
+    [Range(0, 1)]
+    public float Volume;
+}
+
+
+
 public class SFX_Light : MonoBehaviour
 {
     [SerializeField] AudioSource lightActivates;
     [SerializeField] AudioSource lightHums;
     [SerializeField] AudioClip[] audioClips;
+    [SerializeField] SFX_AudioClips[] audioClips2;
     
     [SerializeField] LightPositionSlider lightPositionSlider;
     
@@ -25,14 +36,17 @@ public class SFX_Light : MonoBehaviour
 
     private void OnEnable()
     {
-        lightActivates.PlayOneShot(_isRight ? audioClips[5] : audioClips[2]); // Play the sound of the light turning on
-        lightHums.clip = _isRight ? audioClips[3] : audioClips[0];
+        //lightActivates.PlayOneShot(_isRight ? audioClips[5] : audioClips[2]); // Play the sound of the light turning on
+        lightActivates.PlayOneShot(_isRight ? audioClips2[5].Clip : audioClips2[2].Clip);
+        //lightHums.clip = _isRight ? audioClips[3] : audioClips[0];
+        lightHums.clip = _isRight ? audioClips2[3].Clip : audioClips2[0].Clip;
         lightHums.Play();
     }
 
     private void OnDisable()
     {
-        lightActivates.PlayOneShot(_isRight ? audioClips[4] : audioClips[1]);
+        //lightActivates.PlayOneShot(_isRight ? audioClips[4] : audioClips[1]);
+        lightActivates.PlayOneShot(_isRight ? audioClips2[4].Clip : audioClips2[1].Clip);
         lightHums.Stop();
     }
 
