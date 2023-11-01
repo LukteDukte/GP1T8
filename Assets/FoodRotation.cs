724cf7c2753e3bc6dbf368616f98c755f3297b11
PlayerZongying <playerzongying@gmail.com>
2023-11-01 10:00:54 +0100

    food rotating

---
+++ Assets/FoodRotation.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class FoodRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isRandomInitRotaton;
+    public bool isRandomRotationSpeed;
+
+    public Vector3 initEulerAngle;
+    public Vector3 RotationSpeedInEulerAngle;
+    void Start()
+    {
+        if (isRandomInitRotaton)
+        {
+            initEulerAngle = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
+        }
+        
+        if (isRandomRotationSpeed)
+        {
+            RotationSpeedInEulerAngle = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
+        }
+        
+        transform.rotation = Quaternion.Euler(initEulerAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(RotationSpeedInEulerAngle * Time.deltaTime);
+    }
+}
