b3c77ff7a601ee6b3e250fd6c2d18e9669062c00
ElliottFutureGames <elliot.fagerdin.blom@edu.futuregames.nu>
2023-11-02 11:40:38 +0100

    Re fixed ui score texture

--- Assets/_Scripts/GameUIManager.cs
+++ Assets/_Scripts/GameUIManager.cs
@@ -18,6 +18,9 @@ public class GameUIManager : MonoBehaviour
 
     public GameObject timer;
 
+    [SerializeField] GameObject UIYourScoreTexture;
+    [SerializeField] GameObject UIHighScoreTexture;
+
     private void Awake()
     {
         if(Instance == null)
@@ -38,8 +41,8 @@ public class GameUIManager : MonoBehaviour
         noNewHighscore = Highscore.Instance.noNewHighscore;
         NewHighscore = Highscore.Instance.newHighscore;
 
-        noNewHighscore.AddListener(DisplayNoNewHighscore);
-        NewHighscore.AddListener(DisplayNewHighscore);
+        noNewHighscore.AddListener(DisplayYourScore);
+        NewHighscore.AddListener(DisplayYourScore);
     }
 
     private void Update()
@@ -52,11 +55,12 @@ public class GameUIManager : MonoBehaviour
         UIScoreText.text = "Score:" + Volvox.Instance.colonyCenter.childCount.ToString();
     }
 
-    private void DisplayNoNewHighscore()
+    private void DisplayYourScore()
     {
         deathCamera.SetActive(true);
 
-        UIHighscoreText.text = "YOUR SCORE: " + highscoreData.highscore.ToString();
+        UIHighscoreText.text = ": " + highscoreData.highscore.ToString();
+        UIYourScoreTexture.SetActive(true);
 
         //print(GameObject.FindFirstObjectByType<Button>().gameObject.name);
 
@@ -68,7 +72,8 @@ public class GameUIManager : MonoBehaviour
     {
         deathCamera.SetActive(true);
 
-        UIHighscoreText.text = "NEW HIGH SCORE: " + highscoreData.highscore.ToString();
+        UIHighscoreText.text = ": " + highscoreData.highscore.ToString();
+        UIYourScoreTexture.SetActive(true);
 
         //GameObject.FindFirstObjectByType<Button>().gameObject.SetActive(true);
         StartCoroutine(ReturnToTitle(10));
